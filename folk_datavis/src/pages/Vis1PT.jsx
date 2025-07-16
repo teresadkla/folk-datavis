9c516a4d8a59a32079fc5e6d47b1e0736647dafa
teresadkla <teresafernandeslbbh@gmail.com>
2025-07-16 18:53:14 +0100

    navegação sem animações

--- folk_datavis/src/pages/Vis1PT.jsx
+++ folk_datavis/src/pages/Vis1PT.jsx
@@ -1,66 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 import NetworkDiagram from "../components/PT_VIS/network_rootsPT";
 import PortugalMap from "../components/PT_VIS/map";
 import TemaRegiaoVis from "../components/PT_VIS/dotplotPT";
 import SpiralVis from "../components/PT_VIS/spiral";
 import NavigationBar from "../components/nav";
-import VerticalNav from "../components/verticalnav";
 import "../css/PTpages.css";
 
-export default function visPT() {
+export default function VisPT() {
+  const [currentVis, setCurrentVis] = useState(1);
+
+  const handleNext = () => {
+    if (currentVis < 4) setCurrentVis(currentVis + 1);
+  };
+
+  const handlePrev = () => {
+    if (currentVis > 1) setCurrentVis(currentVis - 1);
+  };
+
   return (
     <>
       <NavigationBar />
-
       <div className="visPT-container">
-        {/* <VerticalNav /> */}
 
-        {/* Network Diagram */}
-        <div id="network-section" className="vispt-roots">
-          <NetworkDiagram />
-        </div>
-
-        {/* Descriptive Text */}
-        <div className="roots-description">
-          <h2 className="roots-title">
-            Song variation and propagation through Portuguese territory
-          </h2>
-          <p className="roots-text">
-            Lorem ipsum dolor sit amet consectetur. Pulvinar faucibus non
-            pellentesque hac nisl. Potenti vel sit neque nunc sed fames urna.
-            Tristique risus volutpat viverra nibh phasellus massa magna urna.
-            Non mauris enim mus egestas.
-            <br /><br />
-            Lorem ipsum dolor sit amet consectetur. Pulvinar faucibus non
-            pellentesque hac nisl. Potenti vel sit neque nunc sed fames urna.
-            Tristique risus volutpat viverra nibh phasellus massa magna urna.
-            Non mauris enim mus egestas.
-          </p>
+        {/* VIS 1 */}
+        <div className="vis1" style={{ display: currentVis === 1 ? "contents" : "none" }}>
+          <div id="network-section" className="vispt-roots">
+            <NetworkDiagram />
+          </div>
+          <div className="roots-description">
+            <h2 className="roots-title">Song variation and propagation through Portuguese territory</h2>
+            <p className="roots-text">
+              Lorem ipsum dolor sit amet consectetur. Pulvinar faucibus non pellentesque hac nisl. Potenti vel sit neque nunc sed fames urna.
+              <br /><br />
+              Tristique risus volutpat viverra nibh phasellus massa magna urna. Non mauris enim mus egestas.
+            </p>
+          </div>
         </div>
 
-        {/* Portugal Map */}
-        <div className="map-description">
-          <h2 className="map-title">
-          Number of registers of folk music per district
-          </h2>
-        </div>
-        <div id="map-section" className="visPT-map">
-          <PortugalMap />
-        </div>
-
-        <div className="dotplot-PT-description">
-          <h2 className="dotplot-PT-title">
-         Frequency of songs throught the Portuguese Territory
-          </h2>
-        </div>
-        {/* Dotplot */}
-        <div id="dotplot-section" className="visPT-dotplot">
-          <TemaRegiaoVis />
+        {/* VIS 2 */}
+        <div className="vis2" style={{ display: currentVis === 2 ? "contents" : "none" }}>
+          <div className="map-description">
+            <h2 className="map-title">Number of registers of folk music per district</h2>
+          </div>
+          <div id="map-section" className="visPT-map">
+            <PortugalMap />
+          </div>
         </div>
 
-        {/* Spiral */}
-        <div id="spiral-section" className="visPT-spiral">
-          <SpiralVis />
+        {/* VIS 3 */}
+        <div className="vis3" style={{ display: currentVis === 3 ? "contents" : "none" }}>
+          <div className="dotplot-PT-description">
+            <h2 className="dotplot-PT-title">Frequency of songs throughout the Portuguese Territory</h2>
+          </div>
+          <div id="dotplot-section" className="visPT-dotplot">
+            <TemaRegiaoVis />
+          </div>
+        </div>
+
+        {/* VIS 4 */}
+        <div className="vis4" style={{ display: currentVis === 4 ? "contents" : "none" }}>
+          <div id="spiral-section" className="visPT-spiral">
+            <SpiralVis />
+          </div>
+        </div>
+
+        {/* NAVIGATION BUTTONS */}
+        <div className="nav-buttons">
+          {currentVis > 1 && (
+            <button onClick={handlePrev} className="nav-btn up">↑</button>
+          )}
+          {currentVis < 4 && (
+            <button onClick={handleNext} className="nav-btn down">↓</button>
+          )}
         </div>
       </div>
     </>
--- folk_datavis/src/pages/VisIE.jsx
+++ folk_datavis/src/pages/VisIE.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import NetworkDiagramIE from "../components/IE_VIS/network_rootsIE";
 import DotHeatmap from "../components/IE_VIS/dotheatmap";
 import MidiHeatmapComparison from "../components/IE_VIS/dotplotcomparison";
@@ -7,7 +7,16 @@ import NavigationBar from "../components/nav";
 import VerticalNav from "../components/verticalnav";
 import "../css/IEpages.css";
 
-export default function visPT() {
+export default function VisIE() {
+    const [currentVis, setCurrentVis] = useState(1);
+
+    const handleNext = () => {
+        if (currentVis < 4) setCurrentVis(currentVis + 1);
+    };
+
+    const handlePrev = () => {
+        if (currentVis > 1) setCurrentVis(currentVis - 1);
+    };
     return (
         <>
             <NavigationBar />
@@ -15,57 +24,82 @@ export default function visPT() {
             <div className="visIE-container">
                 {/* <VerticalNav /> */}
 
-                {/* Descriptive Text */}
-                <div className="roots-description-IE">
-                    <h2 className="roots-title-IE">
-                    Song variation considering the change in the musical Mode
-                    </h2>
-                    <p className="roots-text-IE">
-                        Lorem ipsum dolor sit amet consectetur. Pulvinar faucibus non
-                        pellentesque hac nisl. Potenti vel sit neque nunc sed fames urna.
-                        Tristique risus volutpat viverra nibh phasellus massa magna urna.
-                        Non mauris enim mus egestas.
-                        <br /><br />
-                        Lorem ipsum dolor sit amet consectetur. Pulvinar faucibus non
-                        pellentesque hac nisl. Potenti vel sit neque nunc sed fames urna.
-                        Tristique risus volutpat viverra nibh phasellus massa magna urna.
-                        Non mauris enim mus egestas.
-                    </p>
-                </div>
-                {/* Network Diagram */}
-                <div id="network-IE-section" className="visIE-roots">
-                    <NetworkDiagramIE />
-                </div>
-
-                {/* Heatmap dots */}
-                <div className="Types-description">
-                    <h2 className="Types-title">
-                    Types of Irish folk 
-                    </h2>
-                    <p className="Types-text-IE">
-                        Lorem ipsum dolor sit amet consectetur. Pulvinar faucibus non
-                        pellentesque hac nisl. Potenti vel sit neque nunc sed fames urna.
-                        Tristique risus volutpat viverra nibh phasellus massa magna urna.
-                        Non mauris enim mus egestas.
-                        <br /><br />
-                        Lorem ipsum dolor sit amet consectetur. Pulvinar faucibus non
-                        pellentesque hac nisl. Potenti vel sit neque nunc sed fames urna.
-                        Tristique risus volutpat viverra nibh phasellus massa magna urna.
-                        Non mauris enim mus egestas.
-                    </p>
-                </div>
-                <div id="heatmap-section" className="heatmap-types">
-                    <DotHeatmap />
+                {/* VIS 1 */}
+                <div className="vis1" style={{ display: currentVis === 1 ? "contents" : "none" }}>
+                    {/* Descriptive Text */}
+                    <div className="roots-description-IE">
+                        <h2 className="roots-title-IE">
+                            Song variation considering the change in the musical Mode
+                        </h2>
+                        <p className="roots-text-IE">
+                            Lorem ipsum dolor sit amet consectetur. Pulvinar faucibus non
+                            pellentesque hac nisl. Potenti vel sit neque nunc sed fames urna.
+                            Tristique risus volutpat viverra nibh phasellus massa magna urna.
+                            Non mauris enim mus egestas.
+                            <br /><br />
+                            Lorem ipsum dolor sit amet consectetur. Pulvinar faucibus non
+                            pellentesque hac nisl. Potenti vel sit neque nunc sed fames urna.
+                            Tristique risus volutpat viverra nibh phasellus massa magna urna.
+                            Non mauris enim mus egestas.
+                        </p>
+                    </div>
+                    {/* Network Diagram */}
+                    <div id="network-IE-section" className="visIE-roots">
+                        <NetworkDiagramIE />
+                    </div>
                 </div>
 
-                {/* Comparison */}
-                <div id="dotplot-comparison-section" className="dotplot-comparison">
-                    <MidiHeatmapComparison />
+
+
+                {/* VIS 2 */}
+                <div className="vis2" style={{ display: currentVis === 2 ? "contents" : "none" }}>
+                    {/* Heatmap dots */}
+                    <div className="Types-description">
+                        <h2 className="Types-title">
+                            Types of Irish folk
+                        </h2>
+                        <p className="Types-text-IE">
+                            Lorem ipsum dolor sit amet consectetur. Pulvinar faucibus non
+                            pellentesque hac nisl. Potenti vel sit neque nunc sed fames urna.
+                            Tristique risus volutpat viverra nibh phasellus massa magna urna.
+                            Non mauris enim mus egestas.
+                            <br /><br />
+                            Lorem ipsum dolor sit amet consectetur. Pulvinar faucibus non
+                            pellentesque hac nisl. Potenti vel sit neque nunc sed fames urna.
+                            Tristique risus volutpat viverra nibh phasellus massa magna urna.
+                            Non mauris enim mus egestas.
+                        </p>
+                    </div>
+                    <div id="heatmap-section" className="heatmap-types">
+                        <DotHeatmap />
+                    </div>
                 </div>
 
-                {/* Pitch */}
-                <div id="picth-section" className="pitch-abc">
-                    <ABCVisualizer />  
+
+                {/* VIS 3 */}
+                <div className="vis3" style={{ display: currentVis === 3 ? "contents" : "none" }}>
+                    {/* Comparison */}
+                    <div id="dotplot-comparison-section" className="dotplot-comparison">
+                        <MidiHeatmapComparison />
+                    </div>
+                </div>
+
+
+                {/* VIS 4 */}
+                <div className="vis4" style={{ display: currentVis === 4 ? "contents" : "none" }}>
+                    {/* Pitch */}
+                    <div id="picth-section" className="pitch-abc">
+                        <ABCVisualizer />
+                    </div>
+                </div >
+                {/* NAVIGATION BUTTONS */}
+                <div className="nav-buttons">
+                    {currentVis > 1 && (
+                        <button onClick={handlePrev} className="nav-btn up">↑</button>
+                    )}
+                    {currentVis < 4 && (
+                        <button onClick={handleNext} className="nav-btn down">↓</button>
+                    )}
                 </div>
             </div>
         </>
